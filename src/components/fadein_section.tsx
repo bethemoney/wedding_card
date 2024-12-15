4808314fc9d5f50d105991c59cb5706b51ef806f
seungjin <bethemoneye@gmail.com>
2024-12-15 21:28:27 +0900

    unused err fix

--- src/components/fadein_section.tsx
+++ src/components/fadein_section.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function FadeInSection(props) {
   const [isVisible, setVisible] = useState(true);
