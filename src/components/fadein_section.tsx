37b549be62e246a7c387f07c8b23dbcf16c2760a
seungjin <bethemoneye@gmail.com>
2024-12-15 21:44:23 +0900

    comment

--- src/components/fadein_section.tsx
+++ src/components/fadein_section.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useState, useEffect } from 'react';
-
-export default function FadeInSection(props: any) {
+// @ts-expect-error: type no define
+export default function FadeInSection(props) {
   const [isVisible, setVisible] = useState(true);
 
   const domRef = React.useRef();
@@ -10,14 +10,14 @@ export default function FadeInSection(props: any) {
     const observer = new IntersectionObserver(entries => {
       entries.forEach(entry => setVisible(entry.isIntersecting));
     });
-    // @ts-expect-error: 
+    // @ts-expect-error: type no define
     observer.observe(domRef.current);
-    // @ts-expect-error: 
+    // @ts-expect-error: type no define
     return () => observer.unobserve(domRef.current);
   }, []);
 
   return (
-    // @ts-expect-error: 
+    // @ts-expect-error: type no define
     <div ref={domRef} className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}>
       {props.children}
     </div>
