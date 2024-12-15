1fdc8cb509b950e637ebf98cc59382d5c454f4e1
seungjin <bethemoneye@gmail.com>
2024-12-15 21:40:33 +0900

    에러 lint

--- src/components/fadein_section.tsx
+++ src/components/fadein_section.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState, useEffect } from 'react';
-/* eslint-disable  no-explicit-any */
+
 export default function FadeInSection(props: any) {
   const [isVisible, setVisible] = useState(true);
 
@@ -10,14 +10,14 @@ export default function FadeInSection(props: any) {
     const observer = new IntersectionObserver(entries => {
       entries.forEach(entry => setVisible(entry.isIntersecting));
     });
-    // @ts-ignore: 
+    // @ts-expect-error: 
     observer.observe(domRef.current);
-    // @ts-ignore: 
+    // @ts-expect-error: 
     return () => observer.unobserve(domRef.current);
   }, []);
 
   return (
-    // @ts-ignore: 
+    // @ts-expect-error: 
     <div ref={domRef} className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}>
       {props.children}
     </div>
